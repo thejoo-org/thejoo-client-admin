43737681b84c988d75807b77b68f5840bb0bf808
ro.stradamus <rolee0429@gmail.com>
2022-02-15 02:28:05 +0900

    Init Project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    // 'airbnb',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+}
